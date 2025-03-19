7f2e7665a0b0002b9173a1347a6f247f20fbfac8
oaDaramola <oluwatosinhephzibah@gmail.com>
2025-03-19 23:29:38 +0100

    project setup

---
+++ vite.config.js
@@ -0,0 +1,37 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), eslint(), VitePWA({
+    registerType: 'autoupdate',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'furniture-e-commerce',
+      short_name: 'furniture-e-commerce',
+      description: 'furniture-e-commerce',
+      theme_color: '#ffffff',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: true,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+    
+  })],
+})
