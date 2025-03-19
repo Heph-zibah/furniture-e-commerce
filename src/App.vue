9a852cfb40e70b4047d6c52da755c62f8cfde54b
oaDaramola <oluwatosinhephzibah@gmail.com>
2025-03-20 00:04:55 +0100

    Chore: added the favicon

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,10 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import PWABadge from './components/PWABadge.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/favicon.svg" class="logo" alt="furniture-e-commerce logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="furniture-e-commerce" />
-  <PWABadge />
+  <h1>hello</h1>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
