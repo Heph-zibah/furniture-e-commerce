9196d51c5adcceb107ac4af8eb7294123f680055
oaDaramola <oluwatosinhephzibah@gmail.com>
2025-03-20 01:24:32 +0100

    CHORE: Added the product data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,5 @@ import NavbarComponent from '../components/NavbarComponent.vue';
 </script>
 
 <template>
-    <h1>hello</h1>
-    <router-link :to="{ name: 'ShopView'}">Shop</router-link>
     <navbar-component />
 </template>
